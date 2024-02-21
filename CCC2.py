c3dc85c5b2ca42b462c85b8129e7c46a935149f5
MarcusStorjohann <144149557+MarcusStorjohann@users.noreply.github.com>
2024-02-21 14:46:18 +0000

    done3

--- CCC2.py
+++ CCC2.py
@@ -2,8 +2,10 @@ dusa = int(input())
 yubas_line = []
 
 for i in range(0,6):
-    new_yubas = int(input())
-    yubas_line.append(new_yubas)
+    new_yubas = input()
+    if new_yubas == '':
+        new_yubas = 0
+    yubas_line.append(int(new_yubas))
 
 for i in yubas_line:
     if dusa > i:
